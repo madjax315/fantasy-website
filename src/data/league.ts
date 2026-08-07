9b0f29f4ec2ee7c845d8e545c687800da0c31333
unknown <efinge1@wgu.edu>
2026-08-06 22:04:09 -0600

    Initial website

---
+++ src/data/league.ts
@@ -0,0 +1,59 @@
+export const league = [
+  {
+    manager: "Evan Finger",
+    nascar: "Ty Dillon",
+
+    keepers: [
+      {
+        player: "Bo Nix",
+        round: 8,
+        years: 2,
+        signed: 2025
+      },
+      {
+        player: "Courtland Sutton",
+        round: 7,
+        years: 2,
+        signed: 2025
+      },
+      {
+        player: "Chris Godwin",
+        round: 9,
+        years: 1,
+        signed: 2026
+      }
+    ],
+
+    contractYearsUsed: 2
+  },
+
+  {
+    manager: "Ryan Finger",
+    nascar: "Erik Jones",
+
+    keepers: [
+      {
+        player: "Jalen Hurts",
+        round: 4,
+        years: 2,
+        signed: 2025
+      },
+      {
+        player: "Ladd McConkey",
+        round: 10,
+        years: 2,
+        signed: 2025
+      },
+      {
+        player: "George Pickens",
+        round: 7,
+        years: 2,
+        signed: 2026
+      }
+    ],
+
+    contractYearsUsed: 4
+  },
+
+  // Continue with every owner...
+]; 
