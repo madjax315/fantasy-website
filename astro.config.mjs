9c1f0e066c9ea9951be987b305255d2930bd06c7
unknown <efinge1@wgu.edu>
2026-08-06 22:29:30 -0600

    Setup GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://madjax315.github.io',
+  base: '/fantasy-website',
+
   vite: {
     plugins: [tailwindcss()]
   }
