6bb6eb503d1a6001a1299b3bbe483003ca2bf239
unknown <efinge1@wgu.edu>
2026-08-08 20:01:25 -0600

    remove last name

--- src/data/league.ts
+++ src/data/league.ts
@@ -1,76 +1,76 @@
 export const league = [
   {
-    manager: "Evan Finger",
+    manager: "Evan",
     nascar: "John Hunter Nemechek",
     qualifyingPosition: 21,
     finishingPosition: null
   },
 
   {
-    manager: "Ryan Finger",
+    manager: "Ryan",
     nascar: "Erik Jones",
     qualifyingPosition: 10,
     finishingPosition: null
   },
 
   {
-    manager: "Wyatt MacGaffey",
+    manager: "Wyatt",
     nascar: "Josh Berry",
     qualifyingPosition: 13,
     finishingPosition: null
   },
 
   {
-    manager: "Colin MacGaffey",
+    manager: "Colin",
     nascar: "Daniel Suarez",
     qualifyingPosition: 33,
     finishingPosition: null
   },
 
   {
-    manager: "Shawn Lenahan",
+    manager: "Shawn",
     nascar: "Michael McDowell",
     qualifyingPosition: 11,
     finishingPosition: null
   },
 
   {
-    manager: "Ryan Keener",
+    manager: "Keen",
     nascar: "Ross Chastain",
     qualifyingPosition: 9,
     finishingPosition: null
   },
 
   {
-    manager: "Jas Gill",
+    manager: "Jaz",
     nascar: "Austin Cindric",
     qualifyingPosition: 12,
     finishingPosition: null
   },
 
   {
-    manager: "Chris Ruiz",
+    manager: "Chris",
     nascar: "AJ Allmendinger",
     qualifyingPosition: 25,
     finishingPosition: null
   },
 
   {
-    manager: "Matt Wynn",
+    manager: "Matt",
     nascar: "Austin Dillon",
     qualifyingPosition: 7,
     finishingPosition: null
   },
 
   {
-    manager: "Devang Patel",
+    manager: "Dev",
     nascar: "Ricky Stenhouse Jr.",
     qualifyingPosition: 16,
     finishingPosition: null
   },
 
   {
-    manager: "Taylor Ewashinka",
+    manager: "Taylor",
     nascar: "Shane van Gisbergen",
     qualifyingPosition: 23,
     finishingPosition: null
