a191574ddf06ca15377d245d2f30d299ca219783
unknown <efinge1@wgu.edu>
2026-08-10 20:32:33 -0600

    added draft board

---
+++ src/data/pre-draft-2026.ts
@@ -0,0 +1,76 @@
+export const draftOrder = [
+  "Josh",
+  "Chris",
+  "Colin",
+  "Devang",
+  "Shawn",
+  "Jas",
+  "Taylor",
+  "Ryan",
+  "Keener",
+  "Evan",
+  "Matt",
+  "Wyatt",
+];
+
+export const keepers: Record<string, Record<number, string>> = {
+  Josh: {},
+
+  Chris: {
+    6: "Emeka Egbuka",
+    8: "Jaxson Dart",
+    9: "Brock Bowers",
+  },
+
+  Colin: {
+    6: "Jayden Daniels",
+  },
+
+  Dev: {
+    3: "Malik Nabers",
+  },
+
+  Shawn: {
+    4: "Josh Allen",
+    7: "Jakobi Meyers",
+  },
+
+  Jas: {
+    7: "Dak Prescott",
+    8: "Tre Tucker",
+  },
+
+  Taylor: {
+    5: "Quinshon Judkins",
+    8: "Jaxon Smith-Njigba",
+    9: "Cam Skattebo",
+  },
+
+  Ryan: {
+    4: "Jalen Hurts",
+    7: "George Pickens",
+    10: "Ladd McConkey",
+  },
+
+  Keen: {
+    6: "Jaylen Waddle",
+    11: "Brian Thomas Jr.",
+    12: "Caleb Williams",
+  },
+
+  Evan: {
+    7: "Courtland Sutton",
+    8: "Bo Nix",
+    9: "Chris Godwin",
+  },
+
+  Matt: {
+    3: "Nico Collins",
+  },
+
+  Wyatt: {
+    3: "De'Von Achane",
+    5: "Tee Higgins",
+    10: "Tucker Kraft",
+  },
+};
